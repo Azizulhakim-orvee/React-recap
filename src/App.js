444f5137368afdd853e3fb45e01adbf8b15002a7
new name <new email>
2021-07-25 23:56:12 +0600

    fist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [nayoks, setNayoks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setNayoks(data));
+  }, []);
+
+
+
+  // const nayoks = [{name:'Shakib khan', age:55}, {name:'wwwwwww', age:null},{name:'ssssssssss', age:55}, {name:'xxxxxxxx', age:55},{name:'Imran Hashmi', age:55}]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <MovieCounter />
+      {nayoks.map((nayok) => (
+        <Nayok
+          name={nayok.name}
+          email={nayok.email}
+          city={nayok.address.city}
+          lat={nayok.address.geo.lat}
+          lng={nayok.address.geo.lng}
+          key={nayok.id}
         >
-          Learn React
-        </a>
-      </header>
+          {" "}
+        </Nayok>
+      ))}
     </div>
   );
 }
+
+const Nayok = ({ name, email, city, lat, lng }) => {
+  const nyokStyle = {
+    border: "2px solid red",
+    margin: "5px",
+  };
+
+  return (
+    <div style={nyokStyle}>
+      <h1>I am {name} </h1>
+      <h3>My email {email || 30} </h3>
+      <h3>I live in {city}</h3>
+      <h3>
+        My geo location latitude :{lat} longitude :{lng}
+      </h3>
+    </div>
+  );
+};
+
+const MovieCounter = () => {
+  const [count, setCount] = useState(5);
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Add Movie</button>
+      <h5>Number of Movies: {count} </h5>
+      <MovieDisplay movies={count + 1.5} />
+    </div>
+  );
+};
+
+const MovieDisplay = ({ movies }) => {
+  return (
+    <div>
+      <h4>I have acted in {movies} movies</h4>
+    </div>
+  );
+};
 
 export default App;
